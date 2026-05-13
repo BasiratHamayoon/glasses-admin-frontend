34f05fa94fc8c212a91e23334ff249f82a16dc98
BasiratHamayoon <basirathumayun@gmail.com>
2026-05-13 15:14:07 +0500

    delete all files

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactCompiler: false,
-  reactStrictMode: false,
-};
-
-export default nextConfig;
--- redux/actions/pricingActions.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import api from '@/lib/api';
-
-export const fetchPricingRules = createAsyncThunk('pricing/fetchAll', async () => {
-  // Using offers based on your backend routes file
-  const response = await api.get('/admin/offers'); 
-  return response.data.data.offers || response.data.data;
-});
